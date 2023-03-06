337cf37e0d0a3a7a60a2ff60ec3cdc43c4c4d382
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-03-06 14:53:30 +0530

    Add files via upload

---
+++ SearchPerson.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AddressBookManagementSystem
+{
+    public class SearchPerson
+    {
+        static string GetState(string city)
+        {
+            switch (city)
+            {
+                case "New York":
+                    return "New York";
+                case "Los Angeles":
+                    return "California";
+                case "Chicago":
+                    return "Illinois";
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
