abe686ccb55751202c0d7aa9c56952b2b31f7e56
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-03-06 14:48:50 +0530

    Add files via upload

---
+++ NoDuplicateEntrySamePerson.cs
@@ -0,0 +1,44 @@
+﻿
+class NoDuplicateEntrySamePerson
+{
+    public string Name { get; set; }
+    public string City { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        NoDuplicateEntrySamePerson other = (NoDuplicateEntrySamePerson)obj;
+        return Name == other.Name && City == other.City;
+    }
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode() ^ City.GetHashCode();
+    }
+}
+
+class AddressBook
+{
+    private List<NoDuplicateEntrySamePerson> people = new List<NoDuplicateEntrySamePerson>();
+
+    public void AddPerson(NoDuplicateEntrySamePerson person)
+    {
+        if (people.Any(p => p.Equals(person)))
+        {
+            Console.WriteLine($"Duplicate entry found for {person.Name} in {person.City}.");
+        }
+        else
+        {
+            people.Add(person);
+            Console.WriteLine($"Added {person.Name} to {person.City}.");
+        }
+    }
+
+    public List<NoDuplicateEntrySamePerson> SearchByName(string name)
+    {
+        return people.Where(p => p.Name == name).ToList();
+    }
+}
