1998a047eebae9d911d2a93fd43302e5d5f634ce
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-03-06 15:01:04 +0530

    Add files via upload

---
+++ alphabeticallyPersonName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+public class Person7
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Phone Number: {PhoneNumber}";
+    }
+}
